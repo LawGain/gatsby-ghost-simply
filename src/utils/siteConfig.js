19ec26b354bd6a3c3746737930a8a3d7853a7313
Jewgeni Lewash <j.lewash@gmail.com>
2021-11-08 23:17:48 +0100

    update for deployn

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-    siteUrl: `https://gatsby-ghost-simply.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://deployn.de`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Blog`, // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Deployn Blog`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Thoughts, stories and ideas.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost Blog`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Deployn Blog`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
@@ -181,4 +181,4 @@ module.exports = {
             },
         },
     },
-}
+};
